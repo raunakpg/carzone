7fe47bf8cd404edcb2ca4ce050aba68cee11ff33
raunakpg <raunakpg777@gmail.com>
2022-05-30 23:14:03 +0530

    completed teams model

--- carzone/settings.py
+++ carzone/settings.py
@@ -74,10 +74,19 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite'),
+    #}
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone_db',
+        'USER': 'postgres',
+        'PASSWORD' : 'desktop',
+        'HOST': 'localhost',
     }
 }
 
@@ -123,3 +132,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
 os.path.join(BASE_DIR,'carzone/static'),
 ]
+
+#media settings
+
+MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
+MEDIA_URL='/media/'
